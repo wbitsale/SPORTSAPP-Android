1c91692fca16f598ef5db7c956d7df9ff1ee1858
STAR!!! <super.star99999@yandex.com>
2017-06-27 17:19:59 -0700

    improve monitor page, menu

--- app/src/main/java/com/calm_health/sports/ActivityLogin.java
+++ app/src/main/java/com/calm_health/sports/ActivityLogin.java
@@ -2,12 +2,58 @@ package com.calm_health.sports;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class ActivityLogin extends AppCompatActivity {
+    Button btn_login;
+    LinearLayout lyt_facebook, lyt_google;
+    EditText et_email, et_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initView();
+    }
+
+    public void initView(){
+        btn_login = (Button) findViewById(R.id.btn_login);
+        lyt_facebook = (LinearLayout) findViewById(R.id.btn_sign_in_fb);
+        lyt_google = (LinearLayout) findViewById(R.id.btn_sign_in_google);
+        et_email = (EditText) findViewById(R.id.edt_email);
+        et_pass = (EditText) findViewById(R.id.edt_password);
+//        btn_login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String strEmail = et_email.getText().toString();
+//                String strPass = et_pass.getText().toString();
+//                if(strEmail.length() > 0 && strEmail.length() > 0) {
+//                    if (!strEmail.contains("@")) {
+//                        Toast.makeText(ActivityLogin.this, "Wrong Email Type.", Toast.LENGTH_SHORT).show();
+//                        return;
+//                    }
+//                    else if(!(strPass.length() > 5))
+//                    {
+//                        Toast.makeText(ActivityLogin.this, "Wrong Password.", Toast.LENGTH_SHORT).show();
+//                        return;
+//                    }
+//                    else{
+//                        auth(strEmail, strPass);
+//                    }
+//                }
+//                else{
+//                    Toast.makeText(ActivityLogin.this, "Please Fill the Gaps.", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+    }
+
+    public void auth(String strEmail, String strPass)
+    {
+
     }
 }
--- app/src/main/java/com/calm_health/sports/MonitorActivity.java
+++ app/src/main/java/com/calm_health/sports/MonitorActivity.java
@@ -26,6 +26,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 import android.view.MenuItem;
@@ -71,9 +72,16 @@ public class MonitorActivity extends AppCompatActivity implements BluetoothAdapt
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitor);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-
+        drawer.setScrimColor(Color.TRANSPARENT);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
@@ -205,10 +213,7 @@ public class MonitorActivity extends AppCompatActivity implements BluetoothAdapt
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
-
-                mHrtChart.update();
-
+               mHrtChart.update();
             }
         }, 1000);
     }
